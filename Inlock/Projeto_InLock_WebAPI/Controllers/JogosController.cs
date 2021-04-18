1dea519bf67cee3bef2f65dda6c1c56894e069ac
bielvdm <gabrielvdm2010@hotmail.com>
2021-04-18 18:39:00 -0300

    falta terminar o jwt mas to com preguiça

---
+++ Inlock/Projeto_InLock_WebAPI/Controllers/JogosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_InLock_WebAPI.Domains;
+using Projeto_InLock_WebAPI.Interfaces;
+using Projeto_InLock_WebAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_InLock_WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        private IJogosRepository _JogosRepository { get; set; }
+
+        public JogosController()
+        {
+            _JogosRepository = new JogosRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ListarGet()
+        {
+            List<JogosDomain> Listando = _JogosRepository.ListarJogos();
+
+            return Ok(Listando);
+        }
+
+        
+        [HttpPost]
+        public IActionResult CadastrarPost(JogosDomain NovoJogo)
+        {
+            _JogosRepository.Cadastrar(NovoJogo);
+
+            return StatusCode(201);
+        }
+    }
+}
